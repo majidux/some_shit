e015018e0c4931bafe3809f777de009090133e65
Majid <majiddarvishnejad93@gmail.com>
2021-11-20 13:05:54 +0330

    fix: delete duplicate part

--- index2.js
+++ index2.js
@@ -1,24 +1,4 @@
-function solve({ url, params, query }) {
-  const paramKey = Object.keys(params);
-  const splitUrl = url.split(':').join('').split('/');
-  let j = '';
-  const rest = [];
-  const newUrl = Object.keys(query || {})
-    .map((key) => key + '=' + query[key])
-    .join('&');
-  const newSplitUrl = splitUrl
-    .map((key, index) => {
-      if (index >= 3) {
-        return key;
-      }
-      rest.push(key);
-    })
-    .filter((el) => el !== undefined);
-  for (let i in params) {
-    j = newSplitUrl.map((item) => params[item]).join('&');
-  }
-  return `${rest.join('/')}/${params.id}${newUrl && '/?'}${newUrl}`;
-}
+function solve({ url, params, query }) {}
 
 /// /////////////////////////////////////////////////
 /// ///////// DO NOT CHANGE FOLLOWING ///////////////
